cbd0533aa192882789ce511c68e13eeb02186919
Jullio Cesar <134459685+JullioDutra@users.noreply.github.com>
2026-03-13 19:53:04 -0300

    Update AuthContext.jsx

--- Pokedex/src/context/AuthContext.jsx
+++ Pokedex/src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
 import { loginRequest } from "../services/authService";
 
 export const AuthContext = createContext({});
